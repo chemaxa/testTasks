1a2ad3ebfb0c0c191dd14fb4e9748ec38289b396
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-03-27 23:17:32 +0300

    Готова основная логика. Осталось добавить работу с localstorage

--- bookList/app/app.js
+++ bookList/app/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
+
 /**
+ * 
  * @param {Object}  Config - application config
  * App constructor
  */
@@ -8,33 +10,30 @@ var App = function() {
     var key,
         instance;
 
-    function App(config) {
+    document.addEventListener("DOMContentLoaded", init);
+
+    function init() {
+        if (instance) {
+            instance.service.eventEmmitter("App", "Init", {});
+        } else {
+            console.log("App is not runnig yet");
+        }
+    }
+
+    function App() {
         if (instance) {
             return instance;
         }
 
-
-        if (!config) {
-            throw new Error("Не задана конфигурация приложения");
-        }
-
-        var propertyNames = ['form', 'list', 'app'],
-            i = 0;
-
-        propertyNames.forEach(function(item, i) {
-            if (!config[item]) throw new Error("Не задано свойство: " + item);
-        });
-
         // Singleton of App
         instance = this;
 
-        this.form = document.querySelector('[data-app-form=' + config.form + ']');
-        this.list = document.querySelector('[data-app-list=' + config.list + ']');
-        this.container = document.querySelector('[data-app-container=' + config.app + ']');
-        this.inputs = {};
-        for (key in config.inputs) {
-            this.inputs[key] = this.form.querySelector('[data-app-input=' + key + ']');
-        }
+        var container = this.container = document.querySelector('[data-app-container]');
+        if (!container) throw new Error("Нет контейнера приложения");
+        this.form = container.querySelector('[data-app-form]');
+        if (!this.form) throw new Error("Нет формы добавления элементов");
+        this.list = container.querySelector('[data-app-list]');
+        if (!this.list) throw new Error("Нет списка элементов");
 
         /**
          * Set "this" in Controller, Model,  View  to our app instance
@@ -80,38 +79,31 @@ App.prototype.service = function() {
          * @param  {string} - Name of application event (create, read, update ...)
          * @param  {any} - Data that will be added to event 
          */
-    var eventEmmitter = function(name, data) {
-        var appEvent = new CustomEvent(self.constructor.name + ":" + name, {
+    var eventEmmitter = function(initiator, name, data) {
+        var appEvent = new CustomEvent(self.constructor.name + ":" + initiator + ":" + name, {
             bubbles: true,
             detail: data
         });
         self.container.dispatchEvent(appEvent);
     };
-
-    var formValidator = function(form) {
-        var key;
-        var inputs = form.querySelectorAll('input');
-        console.dir(inputs);
-        for (var i = inputs.length - 1; i >= 0; i--) {
-            if (!inputs[i].value) {
-                self.service.eventEmmitter("formValidateError", {
-                    message: "Не заполнено поле " + inputs[i].placeholder // Да брать данные из HTML не комильфо, но писать для тестового задания список сообщений просто лень
-                });
-            }
-        }
+    var messages = {
+        createItemSuccess: "Элемент успешно создан",
+        createItemFailure: "Элемент c указанными данными уже существует",
+        updateItemSuccess: "Элемент успешно обновлен",
+        updateItemFailure: "Элемент c указанными данными уже существует",
     }
-
     return {
         createId: createId,
         appError: appError,
         eventEmmitter: eventEmmitter,
-        formValidate: formValidator
+        messages: messages
     }
 };
 
 
 App.prototype.Controller = function() {
-    var self = this;
+    var self = this,
+        initiator = "controller";
 
     this.form.addEventListener('submit', formHandler, false);
     this.list.addEventListener('click', listHandler, false);
@@ -122,8 +114,7 @@ App.prototype.Controller = function() {
         var action = event.target.dataset.appAction,
             id = event.target.closest('[data-app-item-id]').dataset.appItemId;
         // Создаем свое событие о том что у нас есть запрос на изменение/удаление
-        console.log(self);
-        self.service.eventEmmitter(action, {
+        self.service.eventEmmitter(initiator, action, {
             nativeEvent: event,
             id: id
         });
@@ -132,80 +123,128 @@ App.prototype.Controller = function() {
 
     function formHandler(event) {
         event.preventDefault();
-        console.log(self)
-        return false;
-        var appItem = {
-            propName: event.target.querySelector('[data-app-input=' + self.input + ']').value,
-            pubyear: event.target.querySelector('[data-app-input=pubyear]').value,
-            name: event.target.querySelector('[data-app-input=name]').value,
-            pagenum: event.target.querySelector('[data-app-input=pagenum]').value,
-        };
-        // Создаем свое событие о том что у нас есть новые данные 
-        self.service.eventEmmitter("createItem", {
+        var dataItem = {},
+            i,
+            inputsArr = self.form.querySelectorAll('input');
+        for (i = inputsArr.length - 1; i >= 0; i--) {
+            dataItem[inputsArr[i].name] = inputsArr[i].value;
+            inputsArr[i].value = '';
+        }
+        self.service.eventEmmitter(initiator, "createItem", {
             nativeEvent: event,
-            appItem: appItem
+            dataItem: dataItem
         });
-
     }
 };
 
 
 App.prototype.Model = function() {
-    var self = this;
+    var self = this,
+        initiator = "model",
+        publisher = "controller",
+        state;
+
+
     var dataList = {
-        "a5ol5": {
+        "26xj": {
             author: "Булгаков М.А.",
-            pubyear: 1966,
+            pubyear: "1966",
             name: "Мастер и Маргарита",
-            pagenum: 451
+            pagenum: "451",
+            id: "26xj"
         }
     };
 
-    function getAllItems(argument) {
+    function getAllItems() {
         return dataList;
     }
 
-    function getItem(argument) {
-        return dataList[argument];
+    function getItem(id) {
+        return dataList[id];
     }
 
-    function setItem(argument) {
-
-        var id = app.service.createId(argument.name + argument.pubyear),
+    function createItem(dataItem) {
+        var id = app.service.createId(dataItem.name + dataItem.pubyear + dataItem.author + dataItem.pagenum),
             i;
-        console.log(dataList);
         if (!!dataList[id]) {
             return false;
         }
-
-        dataList[id] = argument;
-        console.log(dataList);
-        return id;
+        dataItem.id = id;
+        dataList[id] = dataItem;
+        return dataItem;
     }
-    //Подписываемся на события
-    this.container.addEventListener(this.constructor.name + ':createItem', function(event) {
-        var id = setItem(event.detail.appItem);
-        console.log(id);
-        if (id) {
-            self.service.eventEmmitter("createItemSuccess", {
-                message: "Элемент успешно создан",
-                id: id
+
+    function updateItem(dataItem) {
+        var previousItemId = dataItem.id;
+        var dataItem = createItem(dataItem);
+        if (!dataItem) return false;
+        deleteItem(previousItemId);
+        return dataItem;
+    }
+
+    function deleteItem(id) {
+        delete dataList[id];
+    }
+
+    this.container.addEventListener(this.constructor.name + ':App:Init', function(event) {
+        var status = "success";
+        self.service.eventEmmitter(initiator, "readAllItems", {
+            dataList: dataList,
+            status: status
+        });
+    });
+
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':createItem', function(event) {
+        var dataItem, message, status, id, previousItemId;
+        if (event.detail.dataItem.id) {
+            previousItemId = event.detail.dataItem.id;
+            dataItem = updateItem(event.detail.dataItem);
+            message = (dataItem.id) ? self.service.messages.updateItemSuccess : self.service.messages.updateItemFailure;
+            status = (dataItem.id) ? "success" : "failure";
+            console.log(dataList);
+            self.service.eventEmmitter(initiator, "updateItem", {
+                dataItem: dataItem,
+                message: message,
+                status: status,
+                previousItemId: previousItemId
             });
+
         } else {
-            self.service.eventEmmitter("createItemError", {
-                message: "Такой элемент уже существует",
+            dataItem = createItem(event.detail.dataItem);
+            id = dataItem.id;
+            status = (id) ? "success" : "failure";
+            message = (id) ? self.service.messages.createItemSuccess : self.service.messages.createItemFailure;
+
+            self.service.eventEmmitter(initiator, "createItem", {
+                message: message,
+                dataItem: dataItem,
+                status: status
             });
         }
+    });
 
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':readItem', function(event) {
+        var id = event.detail.id;
+        self.service.eventEmmitter(initiator, "readItem", {
+            dataItem: dataList[id],
+            status: "success"
+        });
     });
-    this.container.addEventListener(this.constructor.name + ':readItem', function(event) {
-        console.log(event.type);
+
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':editItem', function(event) {
+        var id = event.detail.id;
+        self.service.eventEmmitter(initiator, "editItem", {
+            dataItem: dataList[id],
+            status: "success"
+        });
     });
-    this.container.addEventListener(this.constructor.name + ':updateItem', function(event) {
-        console.log(event.type);
-    });
-    this.container.addEventListener(this.constructor.name + ':deleteItem', function(event) {
-        console.log(event.type);
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':deleteItem', function(event) {
+        var id = event.detail.id;
+        deleteItem(id);
+        self.service.eventEmmitter(initiator, "deleteItem", {
+            dataId: event.detail.id,
+            status: "success"
+        });
     });
     return {
         dataList: dataList
@@ -213,10 +252,85 @@ App.prototype.Model = function() {
 };
 
 App.prototype.View = function() {
-    this.container.addEventListener(this.constructor.name + ':createItemSuccess', function(event) {
-        console.log(event.type);
-    });
-    this.container.addEventListener(this.constructor.name + ':createItemError', function(event) {
-        console.log(event.type, event.detail.message);
-    });
+    var self = this,
+        publisher = "model";
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':createItem', createItemEventHandler);
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':deleteItem', deleteItemEventHandler);
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':editItem', editItemEventHandler);
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':updateItem', updateItemEventHandler);
+    this.container.addEventListener(this.constructor.name + ':' + publisher + ':readAllItems', readAllItemsEventHandler);
+
+    function editItemEventHandler(event) {
+        var dataItem = event.detail.dataItem,
+            inputsArr = self.form.querySelectorAll('input'),
+            i;
+        for (i = inputsArr.length - 1; i >= 0; i--) {
+            inputsArr[i].value = dataItem[inputsArr[i].name];
+        }
+    }
+
+    function updateItemEventHandler(event) {
+        var dataItem = event.detail.dataItem;
+        var isSuccess = (event.detail.status == "success") ? true : false;
+        addMessageToHTML(event.detail.message, isSuccess);
+        console.log(event.detail);
+        if (!isSuccess) return;
+        updateItem(event.detail, isSuccess);
+    }
+
+    function updateItem(data) {
+        var itemEl = self.list.querySelector('[data-app-item-id="' + data.previousItemId + '"]');
+        itemEl.querySelector('[data-app-item-name]').textContent = data.dataItem.name;
+        itemEl.querySelector('[data-app-item-author]').textContent = data.dataItem.author;
+        itemEl.dataset.appItemId = data.dataItem.id;
+    }
+
+    function readAllItemsEventHandler(event) {
+        var dataList = event.detail.dataList,
+            key;
+        for (key in dataList) {
+            addItemToHTML(dataList[key])
+        }
+    }
+
+    function createItemEventHandler(event) {
+        var isSuccess = (event.detail.status == "success") ? true : false;
+        addMessageToHTML(event.detail.message, isSuccess);
+        if (isSuccess) addItemToHTML(event.detail.dataItem);
+    }
+
+    function addMessageToHTML(message, isSuccess) {
+        var messageEl = self.container.querySelector("[data-app-message]"),
+            messageClassName = isSuccess ? "alert-success" : "alert-danger";
+        if (!messageEl) {
+            messageEl = '<div class="alert ' + messageClassName + '"  role="alert" data-app-message="message">' +
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                '<span aria-hidden="true">&times;</span></button>' +
+                '<div class="alert__message" data-app-message-text="message-text">' + message + '</div></div>';
+            self.form.insertAdjacentHTML("afterBegin", messageEl);
+        } else {
+            messageEl.querySelector("[data-app-message-text]").textContent = message;
+        }
+        if (isSuccess) {
+            $(messageEl).closest(".alert").removeClass("alert-danger");
+            $(messageEl).closest(".alert").addClass("alert-success");
+        } else {
+            $(messageEl).closest(".alert").removeClass("alert-success");
+            $(messageEl).closest(".alert").addClass("alert-danger");
+        }
+    }
+
+    function addItemToHTML(item) {
+        var dataItemHTML = '<div href="javascript:void(0)" class="list-group-item" data-app-item-id="' + item.id + '">' +
+            '<h4 class="list-group-item-heading" data-app-item-name>' + item.name + '</h4>' +
+            '<p class="list-group-item-text" data-app-item-author>' + item.author + '</p>' +
+            '<a href="javascript:void(0)" class="label label-primary" data-app-action="editItem">Изменить</a>' +
+            '<a href="javascript:void(0)" class="label label-danger" data-app-action="deleteItem">Удалить</a></div>';
+        self.list.insertAdjacentHTML("afterBegin", dataItemHTML);
+    }
+
+    function deleteItemEventHandler(event) {
+        if (event.detail.status == "success")
+            $('[data-app-item-id=' + event.detail.dataId + ']').remove();
+    }
 };
