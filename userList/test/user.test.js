4e017c98876df2c9865686d1cdd4b08eadc4cc88
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-09-17 22:54:32 +0300

    change user test

--- userList/test/user.test.js
+++ userList/test/user.test.js
@@ -1,12 +1,12 @@
 import User from '../app/user';
 import {assert} from 'chai';
 let user = new User(); 
-console.log(user,User);
+
 describe('User ', () => {
   it('is object',  () =>{
     assert.isObject(user, 'user is object');
   });
-  it('and should have log method',  () =>{
+  it('should have log method',  () =>{
     assert.property(user,'log');
   });
 });
