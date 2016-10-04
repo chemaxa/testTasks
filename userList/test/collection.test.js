876271be5c545cb88aca5f9be42cc0761b1d826e
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-10-04 17:42:14 +0300

    Remove unnecessary checks

--- userList/test/collection.test.js
+++ userList/test/collection.test.js
@@ -1,9 +1,14 @@
 import Collection from '../app/collection';
-import {assert} from 'chai';
+import {
+  assert
+} from 'chai';
 
-import {getLogger} from 'log4js';
-let  logger = getLogger();
-let collection = new Collection(logger);
+import {
+  getLogger
+} from 'log4js';
+let logger = getLogger();
+let collection = new Collection();
+
 let mockValidUser = {
   first_name: "FirstName",
   last_name: "LastName",
@@ -30,9 +35,6 @@ describe('Collection', () => {
   describe('Add item to collection', () => {
     it('Success must return added object', () => {
       assert.deepEqual(collection.addItem(mockValidUser), mockValidUser, 'the objects is not equals');
-    });
-    it('fail add item to collection, must return string with error', () => {
-      assert.isString(collection.addItem(mockValidUser), 'the returned value is not string');
     });
   });
 
@@ -67,39 +69,39 @@ describe('Collection', () => {
 describe('Collection methods forEach, sort(default by login), map.', () => {
   before(() => {
     // runs before all tests in this block
-    mockUpdatedUser.login='alphabet';
+    mockUpdatedUser.login = 'alphabet';
     collection.addItem(mockUpdatedUser);
   });
-  
+
   it('ForEach collections method, receive Function and must call it for each collection item', () => {
-    let i=0;
+    let i = 0;
     collection.forEach(() => i++);
-    assert.equal(i,Object.keys(collection._elems).length, 'the sum of calls callback function is not sum of elems');      
+    assert.equal(i, Object.keys(collection._elems).length, 'the sum of calls callback function is not sum of elems');
   });
 
   it('Map collections method, receive Function and must call it for each collection item, must return new collection', () => {
-    let i=0;
+    let i = 0;
     collection.map((item) => {
-      item['first_name']='tested';
+      item['first_name'] = 'tested';
       i++;
       return item;
     });
-    assert.equal(i,Object.keys(collection._elems).length, 'the sum of calls callback function is not sum of elems');
-    for(let key in collection._elems){
-      assert.equal(collection._elems[key]['first_name'],'tested', 'the name of items is not a "tested"');
+    assert.equal(i, Object.keys(collection._elems).length, 'the sum of calls callback function is not sum of elems');
+    for (let key in collection._elems) {
+      assert.equal(collection._elems[key]['first_name'], 'tested', 'the name of items is not a "tested"');
     }
   });
-  
+
   it(`Sort collections method, receive Function for compare collection items, must return new sorted collection, 
     if function not provided, collection would be sorted by Login`, () => {
     let sorted = collection.sort();
-    let i=0;
-    let previous='';
-    for(let key in collection._elems){
-      if(i){
-        assert.isAbove(previous,collection._elems[key]['login'], 'the login of previous item is not above the login of next item');
+    let i = 0;
+    let previous = '';
+    for (let key in collection._elems) {
+      if (i) {
+        assert.isAbove(previous, collection._elems[key]['login'], 'the login of previous item is not above the login of next item');
       }
-      previous=sorted[key]['login'];
+      previous = sorted[key]['login'];
       i++;
     }
   });
