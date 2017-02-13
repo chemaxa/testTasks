322268512edeabaed338f269ef105aff98aee4b2
Maxim Shkarbalyuk <chemaxa@gmail.com>
2017-02-13 17:16:55 +0300

    Small refactoring

--- userList/app/notificator.js
+++ userList/app/notificator.js
@@ -5,16 +5,30 @@ export default class Notificator {
       "add": "User $login was added"
     };
     this.notifications = Object.assign(defaultNotifications, notifications || {});
-    this.showNotifications = this.showNotifications.bind(this);
+    this.showNotification = this.showNotification.bind(this);
+    this.showCustomNotification = this.showCustomNotification.bind(this);
     this.form = form;
+
   }
 
-  showNotifications(event, item, theme = 'warning') {
+  showCustomNotification(text, theme = 'warning') {
+    let messageEl = `
+      <div class="alert alert-${theme} alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <strong>${theme == 'danger' ? 'Error!' : ''}</strong> text
+      </div>`;
+    this.insertHtml(messageEl);
+  }
+
+  showNotification(event, item, theme = 'warning') {
     let messageEl = `
       <div class="alert alert-${theme} alert-dismissible" role="alert">
           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <strong>${theme == 'danger' ? 'Error!' : ''}</strong> ${this.notifications[event].replace('$login',item.login)}
       </div>`;
-    this.form.formEl.insertAdjacentHTML("afterBegin", messageEl);
+    this.insertHtml(messageEl);
+  }
+  insertHtml (html){
+    this.form.formEl.insertAdjacentHTML("afterBegin", html);
   }
 }
