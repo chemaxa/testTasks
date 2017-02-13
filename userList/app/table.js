d8e1b6a049a719f5ca4c7fe16187154ec6dca2b2
Maxim Shkarbalyuk <chemaxa@gmail.com>
2017-02-13 18:33:15 +0300

    Add HTML  table

--- userList/app/table.js
+++ userList/app/table.js
@@ -20,9 +20,27 @@ export default class Table {
     // 5. **Registered on** — сортируемое
     // 6. **Active** — в формате Yes/No
     let regDate = new Date(user.registered_on);
+    //{1: ‘Administrator’, 2: ‘Technician’, 3:
+    //‘Manager’, 4: ‘Supervisor’}
+    let role;
+    switch (user.role) {
+      case 1:
+        role = 'Administrator';
+        break;
+      case 2:
+        role = 'Technician';
+        break;
+      case 3:
+        role = 'Manager';
+        break;
+      case 4:
+        role = 'Supervisor';
+        break;
+    }
+
     $(this.tableEl).find('tbody').append(
-      `<tr>
-          <td>${user.role}</td>
+      `<tr data-id=${user.login}>
+          <td>${role}</td>
           <td>${user.login}</td>
           <td>${user.first_name} ${user.last_name}</td>
           <td>${user.age}</td>
@@ -31,16 +49,69 @@ export default class Table {
       </tr>`
     );
   }
-  sort(field){
-    function sorter (item1,item2){
+  init() {
+    this.collection.forEach((i, user) => {
+      let role;
+      switch (user.role) {
+        case 1:
+          role = 'Administrator';
+          break;
+        case 2:
+          role = 'Technician';
+          break;
+        case 3:
+          role = 'Manager';
+          break;
+        case 4:
+          role = 'Supervisor';
+          break;
+      }
+      let regDate = new Date(user.registered_on);
+      $(this.tableEl).find('tbody').append(
+        `<tr data-id=${user.login}>
+            <td>${role}</td>
+            <td>${user.login}</td>
+            <td>${user.first_name} ${user.last_name}</td>
+            <td>${user.age}</td>
+            <td>${regDate.getDate()} / ${regDate.getMonth()} / ${regDate.getFullYear()}</td>
+            <td>${user.active}</td>
+        </tr>`
+      );
+    });
+  }
+  sort(field) {
+    function sorter(item1, item2) {
       let param1 = item1[field].toLowerCase();
       let param2 = item2[field].toLowerCase();
       return param1 < param2 ? -1 : (param1 > param2 ? 1 : 0);
     }
     this.collection.sort(sorter);
   }
-  update(){
-    this.collection.forEach();
-    $(this.tableEl).find('tbody').append();
+  update(user) {
+    let regDate = new Date(user.registered_on);
+    let role;
+    switch (user.role) {
+      case 1:
+        role = 'Administrator';
+        break;
+      case 2:
+        role = 'Technician';
+        break;
+      case 3:
+        role = 'Manager';
+        break;
+      case 4:
+        role = 'Supervisor';
+        break;
+    }
+    $(this.tableEl).find(`tbody tr[data-id=${user.login}]`).replaceWith(
+      `<tr data-id=${user.login}>
+          <td>${role}</td>
+          <td>${user.login}</td>
+          <td>${user.first_name} ${user.last_name}</td>
+          <td>${user.age}</td>
+          <td>${regDate.getDate()} / ${regDate.getMonth()} / ${regDate.getFullYear()}</td>
+          <td>${user.active}</td>
+      </tr>`);
   }
 }
