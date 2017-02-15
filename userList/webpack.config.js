2d8f8049d920f58193f94a332f7183649733f6b3
Maxim Shkarbalyuk <chemaxa@gmail.com>
2017-02-15 12:12:43 +0300

    Disable HMR

--- userList/webpack.config.js
+++ userList/webpack.config.js
@@ -6,55 +6,51 @@ module.exports = {
   devtool: 'source-map',
   resolve: {
     extensions: ['', '.js'],
-    moduleDirectories:['node_modules']
+    moduleDirectories: ['node_modules']
   },
   resolveLoader: {
     extensions: ['', '.js'],
-    moduleDirectories:['node_modules'],
-    moduleTemplates:['*-loader']
+    moduleDirectories: ['node_modules'],
+    moduleTemplates: ['*-loader']
   },
-  entry: [
+   entry: [
     './app/main'
   ],
-  output: {
-    path: __dirname+'/public',
+   output: {
+    path: __dirname + '/public',
     filename: 'bundle.js',
     publicPath: '/'
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    //new webpack.HotModuleReplacementPlugin()
   ],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        include: path.join(__dirname, '/app'),
-        query: {
-          presets: ['es2015']
-        },
-        plugins: ['transform-runtime']
-      }
-    ]
-  },
+   module: {   
+    loaders: [{
+      test: /\.js?$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      include: path.join(__dirname, '/app'),
+      query: {
+        presets: ['es2015']
+      },
+      plugins: ['transform-runtime']
+    }] 
+  },
   node: {
     fs: "empty"
   },
   devServer: {
     publicPath: '/',
-    hot:true,
+    hot: false,
     inline: true,
-    contentBase:'./public',
+    contentBase: './public',
     historyApiFallback: true,
-    stats:{
+    stats: {
       colors: true
-    }, 
+    },
     quiet: false,
     noInfo: false,
     port: 1337
   }
 };
-
-
